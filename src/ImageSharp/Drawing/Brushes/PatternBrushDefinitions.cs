b85ec713bf2ec5bcd252ae063d558ae6ad79b0e5
Scott Williams <tocsoft@gmail.com>
2016-12-12 18:54:58 +0000

    some initial patterns

--- src/ImageSharp/Drawing/Brushes/PatternBrushDefinitions.cs
+++ src/ImageSharp/Drawing/Brushes/PatternBrushDefinitions.cs
@@ -13,6 +13,14 @@ namespace ImageSharp.Drawing
 
     public partial class PatternBrush
     {
+        // note 2d arrays when configured using initalizer look inverted
+        // ---> Y axis
+        // ^
+        // | X - axis
+        // |
+        // 
+        //
+
         public static PatternBrush Percent10(Color foreColor)
         {
             return Percent10(foreColor, Color.Transparent);
@@ -34,10 +42,87 @@ namespace ImageSharp.Drawing
         public static PatternBrush Percent20(Color foreColor, Color backColor)
         {
             return new PatternBrush(foreColor, backColor, new bool[,] {
-                { true , false, false, false},
-                { false, false, true , false},
+                { true , false, true , false},
+                { false, false, false, false},
+                { false, true , false, true},
+                { false, false, false, false}
+            });
+        }
+
+
+        public static PatternBrush Horizontal(Color foreColor)
+        {
+            return Horizontal(foreColor, Color.Transparent);
+        }
+        public static PatternBrush Horizontal(Color foreColor, Color backColor)
+        {
+            return new PatternBrush(foreColor, backColor, new bool[,] {
+                { false, true, false, false},
+                { false, true, false, false},
+                { false, true, false, false},
+                { false, true, false, false}
+            });
+        }
+
+
+
+        public static PatternBrush Min(Color foreColor)
+        {
+            return Min(foreColor, Color.Transparent);
+        }
+        public static PatternBrush Min(Color foreColor, Color backColor)
+        {
+            return new PatternBrush(foreColor, backColor, new bool[,] {
+                { false, false, false, true},
+                { false, false, false, true},
+                { false, false, false, true},
+                { false, false, false, true}
+            });
+        }
+
+
+        public static PatternBrush Vertical(Color foreColor)
+        {
+            return Vertical(foreColor, Color.Transparent);
+        }
+
+        public static PatternBrush Vertical(Color foreColor, Color backColor)
+        {
+            return new PatternBrush(foreColor, backColor, new bool[,] {
+                { false, false, false, false},
+                { true , true , true , true },
+                { false, false, false, false},
+                { false, false, false, false}
+            });
+        }
+
+        public static PatternBrush ForwardDiagnal(Color foreColor)
+        {
+            return ForwardDiagnal(foreColor, Color.Transparent);
+        }
+
+        public static PatternBrush ForwardDiagnal(Color foreColor, Color backColor)
+        {
+            return new PatternBrush(foreColor, backColor, new bool[,] {
                 { true, false, false, false},
-                { false, false, true , false}
+                { false, true , false , false },
+                { false, false, true, false},
+                { false, false, false, true}
+            });
+        }
+
+        public static PatternBrush BackwardDiagnal(Color foreColor)
+        {
+            return BackwardDiagnal(foreColor, Color.Transparent);
+        }
+
+        public static PatternBrush BackwardDiagnal(Color foreColor, Color backColor)
+        {
+            return new PatternBrush(foreColor, backColor, new bool[,] {
+                { false, false, false, true},
+                { false, false, true , false},
+                { false, true , false, false},
+                { true,  false, false, false}
             });
         }
     }    
--- tests/ImageSharp.Tests/Drawing/FillPatternTests.cs
+++ tests/ImageSharp.Tests/Drawing/FillPatternTests.cs
@@ -15,102 +15,220 @@ namespace ImageSharp.Tests.Drawing
 
     public class FillPatternBrushTests: FileTestBase
     {
+        private Image Test(string name, Color background, IBrush brush, Color[,] expectedPattern)
+        {
+            string path = CreateOutputDirectory("Fill", "PatternBrush");
+            var image = new Image(20, 20);
+            image
+                  .Fill(background)
+                  .Fill(brush);
+
+            using (FileStream output = File.OpenWrite($"{path}/{name}.png"))
+            {
+                image.Save(output);
+            }
+            using (var sourcePixels = image.Lock())
+            {
+                // lets pick random spots to start checking
+                var r = new Random();
+                var xStride = expectedPattern.GetLength(1);
+                var yStride = expectedPattern.GetLength(0);
+                var offsetX = r.Next(image.Width / xStride) * xStride;
+                var offsetY = r.Next(image.Height / yStride) * yStride;
+                for (var x = 0; x < xStride; x++)
+                {
+                    for (var y = 0; y < yStride; y++)
+                    {
+                        var actualX = x + offsetX;
+                        var actualY = y + offsetY;
+                        var expected = expectedPattern[y, x]; // inverted pattern
+                        var actual = sourcePixels[actualX, actualY];
+                        if (expected != actual)
+                        {
+                            Assert.True(false, $"Expected {expected} but found {actual} at ({actualX},{actualY})");
+                        }
+                    }
+                }
+            }
+            using (FileStream output = File.OpenWrite($"{path}/{name}x4.png"))
+            {
+                image.Resize(80,80).Save(output);
+            }
+
+
+
+            return image;
+        }
+
         [Fact]
         public void ImageShouldBeFloodFilledWithPercent10()
         {
-            string path = CreateOutputDirectory("Fill", "PatternBrush");
-            var image = new Image(500, 500);
-
-            using (FileStream output = File.OpenWrite($"{path}/Percent10.png"))
-            {
-                image
-                    .Fill(Color.Blue)
-                    .Fill(PatternBrush.Percent10(Color.HotPink, Color.LimeGreen))
-                    .Save(output);
-            }
-
-            using (var sourcePixels = image.Lock())
-            {
-                Assert.Equal(Color.HotPink, sourcePixels[0, 0]);
-                Assert.Equal(Color.LimeGreen, sourcePixels[1, 1]);
-                Assert.Equal(Color.HotPink, sourcePixels[2, 2]);
-                Assert.Equal(Color.LimeGreen, sourcePixels[3, 3]);
-            }
+            Test("Percent10", Color.Blue, PatternBrush.Percent10(Color.HotPink, Color.LimeGreen), new Color[,] {
+                { Color.HotPink , Color.LimeGreen, Color.LimeGreen, Color.LimeGreen},
+                { Color.LimeGreen, Color.LimeGreen, Color.LimeGreen, Color.LimeGreen},
+                { Color.LimeGreen, Color.LimeGreen, Color.HotPink , Color.LimeGreen},
+                { Color.LimeGreen, Color.LimeGreen, Color.LimeGreen, Color.LimeGreen}
+            });            
         }
+
         [Fact]
         public void ImageShouldBeFloodFilledWithPercent10Transparent()
         {
-            string path = CreateOutputDirectory("Fill", "PatternBrush");
-            var image = new Image(500, 500);
-
-            using (FileStream output = File.OpenWrite($"{path}/Percent10_Transparent.png"))
-            {
-                image
-                    .BackgroundColor(Color.Blue)
-                    .Fill(PatternBrush.Percent10(Color.HotPink))
-                    .Save(output);
-            }
-
-            using (var sourcePixels = image.Lock())
-            {
-                Assert.Equal(Color.HotPink, sourcePixels[0, 0]);
-                Assert.Equal(Color.Blue, sourcePixels[1, 1]);
-                Assert.Equal(Color.HotPink, sourcePixels[2, 2]);
-                Assert.Equal(Color.Blue, sourcePixels[3, 3]);
-            }
+            Test("Percent10_Transparent", Color.Blue, PatternBrush.Percent10(Color.HotPink), 
+            new Color[,] {
+                { Color.HotPink , Color.Blue, Color.Blue, Color.Blue},
+                { Color.Blue, Color.Blue, Color.Blue, Color.Blue},
+                { Color.Blue, Color.Blue, Color.HotPink , Color.Blue},
+                { Color.Blue, Color.Blue, Color.Blue, Color.Blue}
+            });
         }
 
         [Fact]
         public void ImageShouldBeFloodFilledWithPercent20()
         {
-            string path = CreateOutputDirectory("Fill", "PatternBrush");
-            var image = new Image(500, 500);
-
-            using (FileStream output = File.OpenWrite($"{path}/Percent20.png"))
-            {
-                image
-                    .Fill(Color.Blue)
-                    .Fill(PatternBrush.Percent20(Color.HotPink, Color.LimeGreen))
-                    .Save(output);
-            }
-
-            using (var sourcePixels = image.Lock())
-            {
-                Assert.Equal(Color.HotPink, sourcePixels[0, 0]);
-                Assert.Equal(Color.HotPink, sourcePixels[0, 2]);
-                Assert.Equal(Color.HotPink, sourcePixels[2, 1]);
-                Assert.Equal(Color.HotPink, sourcePixels[2, 3]);
-
-                Assert.Equal(Color.LimeGreen, sourcePixels[0, 1]);
-                Assert.Equal(Color.LimeGreen, sourcePixels[2, 2]);
-            }
+            Test("Percent20", Color.Blue, PatternBrush.Percent20(Color.HotPink, Color.LimeGreen),
+           new Color[,] {
+                { Color.HotPink , Color.LimeGreen, Color.LimeGreen, Color.LimeGreen},
+                { Color.LimeGreen, Color.LimeGreen, Color.HotPink , Color.LimeGreen},
+                { Color.HotPink , Color.LimeGreen, Color.LimeGreen, Color.LimeGreen},
+                { Color.LimeGreen, Color.LimeGreen, Color.HotPink , Color.LimeGreen}
+           });
         }
 
         [Fact]
         public void ImageShouldBeFloodFilledWithPercent20_transparent()
         {
-            string path = CreateOutputDirectory("Fill", "PatternBrush");
-            var image = new Image(500, 500);
-
-            using (FileStream output = File.OpenWrite($"{path}/Percent20_transparent.png"))
-            {
-                image
-                    .Fill(Color.Blue)
-                    .Fill(PatternBrush.Percent20(Color.HotPink))
-                    .Save(output);
-            }
-
-            using (var sourcePixels = image.Lock())
-            {
-                Assert.Equal(Color.HotPink, sourcePixels[0, 0]);
-                Assert.Equal(Color.HotPink, sourcePixels[0, 2]);
-                Assert.Equal(Color.HotPink, sourcePixels[2, 1]);
-                Assert.Equal(Color.HotPink, sourcePixels[2, 3]);
-
-                Assert.Equal(Color.Blue, sourcePixels[0, 1]);
-                Assert.Equal(Color.Blue, sourcePixels[2, 2]);
-
-            }
+            Test("Percent20_Transparent", Color.Blue, PatternBrush.Percent20(Color.HotPink),
+           new Color[,] {
+                { Color.HotPink , Color.Blue, Color.Blue, Color.Blue},
+                { Color.Blue, Color.Blue, Color.HotPink , Color.Blue},
+                { Color.HotPink , Color.Blue, Color.Blue, Color.Blue},
+                { Color.Blue, Color.Blue, Color.HotPink , Color.Blue}
+           });
         }
+
+        [Fact]
+        public void ImageShouldBeFloodFilledWithHorizontal()
+        {
+            Test("Horizontal", Color.Blue, PatternBrush.Horizontal(Color.HotPink, Color.LimeGreen),
+           new Color[,] {
+                { Color.LimeGreen , Color.LimeGreen, Color.LimeGreen, Color.LimeGreen},
+                { Color.HotPink, Color.HotPink, Color.HotPink , Color.HotPink},
+                { Color.LimeGreen , Color.LimeGreen, Color.LimeGreen, Color.LimeGreen},
+                { Color.LimeGreen, Color.LimeGreen, Color.LimeGreen , Color.LimeGreen}
+           });
+        }
+
+        [Fact]
+        public void ImageShouldBeFloodFilledWithHorizontal_transparent()
+        {
+            Test("Horizontal_Transparent", Color.Blue, PatternBrush.Horizontal(Color.HotPink),
+           new Color[,] {
+                { Color.Blue , Color.Blue, Color.Blue, Color.Blue},
+                { Color.HotPink, Color.HotPink, Color.HotPink , Color.HotPink},
+                { Color.Blue , Color.Blue, Color.Blue, Color.Blue},
+                { Color.Blue, Color.Blue, Color.Blue , Color.Blue}
+           });
+        }
+
+
+
+        [Fact]
+        public void ImageShouldBeFloodFilledWithMin()
+        {
+            Test("Min", Color.Blue, PatternBrush.Min(Color.HotPink, Color.LimeGreen),
+           new Color[,] {
+                { Color.LimeGreen , Color.LimeGreen, Color.LimeGreen, Color.LimeGreen},
+                { Color.LimeGreen , Color.LimeGreen, Color.LimeGreen, Color.LimeGreen},
+                { Color.LimeGreen, Color.LimeGreen, Color.LimeGreen , Color.LimeGreen},
+                { Color.HotPink, Color.HotPink, Color.HotPink , Color.HotPink}
+           });
+        }
+
+        [Fact]
+        public void ImageShouldBeFloodFilledWithMin_transparent()
+        {
+            Test("Min_Transparent", Color.Blue, PatternBrush.Min(Color.HotPink),
+           new Color[,] {
+                { Color.Blue , Color.Blue, Color.Blue, Color.Blue},
+                { Color.Blue , Color.Blue, Color.Blue, Color.Blue},
+                { Color.Blue, Color.Blue, Color.Blue , Color.Blue},
+                { Color.HotPink, Color.HotPink, Color.HotPink , Color.HotPink},
+           });
+        }
+
+        [Fact]
+        public void ImageShouldBeFloodFilledWithVertical()
+        {
+            Test("Vertical", Color.Blue, PatternBrush.Vertical(Color.HotPink, Color.LimeGreen),
+           new Color[,] {
+                { Color.LimeGreen, Color.HotPink, Color.LimeGreen, Color.LimeGreen},
+                { Color.LimeGreen, Color.HotPink, Color.LimeGreen, Color.LimeGreen},
+                { Color.LimeGreen, Color.HotPink, Color.LimeGreen, Color.LimeGreen},
+                { Color.LimeGreen, Color.HotPink, Color.LimeGreen, Color.LimeGreen}
+           });
+        }
+
+        [Fact]
+        public void ImageShouldBeFloodFilledWithVertical_transparent()
+        {
+            Test("Vertical_Transparent", Color.Blue, PatternBrush.Vertical(Color.HotPink),
+           new Color[,] {
+                { Color.Blue, Color.HotPink, Color.Blue, Color.Blue},
+                { Color.Blue, Color.HotPink, Color.Blue, Color.Blue},
+                { Color.Blue, Color.HotPink, Color.Blue, Color.Blue},
+                { Color.Blue, Color.HotPink, Color.Blue, Color.Blue}
+           });
+        }
+
+        [Fact]
+        public void ImageShouldBeFloodFilledWithForwardDiagnal()
+        {
+            Test("ForwardDiagnal", Color.Blue, PatternBrush.ForwardDiagnal(Color.HotPink, Color.LimeGreen),
+           new Color[,] {
+                { Color.HotPink, Color.LimeGreen, Color.LimeGreen, Color.LimeGreen},
+                { Color.LimeGreen, Color.HotPink, Color.LimeGreen, Color.LimeGreen},
+                { Color.LimeGreen, Color.LimeGreen, Color.HotPink, Color.LimeGreen},
+                { Color.LimeGreen, Color.LimeGreen, Color.LimeGreen, Color.HotPink}
+           });
+        }
+
+        [Fact]
+        public void ImageShouldBeFloodFilledWithForwardDiagnal_transparent()
+        {
+            Test("ForwardDiagnal_Transparent", Color.Blue, PatternBrush.ForwardDiagnal(Color.HotPink),
+           new Color[,] {
+                { Color.HotPink, Color.Blue,    Color.Blue,    Color.Blue},
+                { Color.Blue,    Color.HotPink, Color.Blue,    Color.Blue},
+                { Color.Blue,    Color.Blue,    Color.HotPink, Color.Blue},
+                { Color.Blue,    Color.Blue,    Color.Blue,    Color.HotPink}
+           });
+        }
+
+        [Fact]
+        public void ImageShouldBeFloodFilledWithBackwardDiagnal()
+        {
+            Test("BackwardDiagnal", Color.Blue, PatternBrush.BackwardDiagnal(Color.HotPink, Color.LimeGreen),
+           new Color[,] {
+                { Color.LimeGreen, Color.LimeGreen, Color.LimeGreen, Color.HotPink},
+                { Color.LimeGreen, Color.LimeGreen, Color.HotPink, Color.LimeGreen},
+                { Color.LimeGreen, Color.HotPink, Color.LimeGreen, Color.LimeGreen},
+                { Color.HotPink, Color.LimeGreen, Color.LimeGreen, Color.LimeGreen}
+           });
+        }
+
+        [Fact]
+        public void ImageShouldBeFloodFilledWithBackwardDiagnal_transparent()
+        {
+            Test("BackwardDiagnal_Transparent", Color.Blue, PatternBrush.BackwardDiagnal(Color.HotPink),
+           new Color[,] {
+                { Color.Blue, Color.Blue,    Color.Blue,    Color.HotPink},
+                { Color.Blue,    Color.Blue, Color.HotPink,    Color.Blue},
+                { Color.Blue,    Color.HotPink,    Color.Blue, Color.Blue},
+                { Color.HotPink,    Color.Blue,    Color.Blue,    Color.Blue}
+           });
+        }
+
+        
     }
 }
